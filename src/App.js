8518e596e0facb2cb7ede87e90f425e431c0ffa7
IbrahimElsharawy <ibrahimelsha33rawy@gmail.com>
2023-01-12 18:07:17 +0200

    Add Folder Structure && Add Navbar Component

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import {Routes , Route} from 'react-router-dom';
+import Home from "./components/Home";
+import Products from "./components/Products";
+import About from "./components/About";
+import Contact from "./components/Contact";
+function App() {
+  return (
+    <div  >
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/products" element={<Products/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+
+      </Routes>
+      
+    </div>
+  );
+}
+
+export default App;
